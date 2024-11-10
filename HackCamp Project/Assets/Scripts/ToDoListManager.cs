843120133680effc039c79374018333d7e500f8a
Eddie Yoshie <eddieyoshie@gmail.com>
2024-11-10 15:39:46 -0800

    Finished mostly

--- HackCamp Project/Assets/Scripts/ToDoListManager.cs
+++ HackCamp Project/Assets/Scripts/ToDoListManager.cs
@@ -5,17 +5,21 @@ using System.Collections;
 
 public class ToDoListManager : MonoBehaviour
 {
-    public TMP_InputField inputField; // Drag your InputField here
-    public Toggle toggle; // Drag the Toggle here
-    public Image imageToChange; // Drag the Image component here
+    public TMP_InputField[] inputFields; // Drag your 10 InputFields here
+    public Toggle[] toggles; // Drag your 10 Toggles here
+    public Image imageToChange; // Drag the single Image component here
     public Sprite originalSprite; // Assign the default image here
     public Sprite newSprite; // Assign the image you want to show temporarily
     public float displayDuration = 2f; // Set how long the new image should display
 
     private void Start()
     {
-        toggle.onValueChanged.AddListener(ToggleStrikethrough);
-        toggle.onValueChanged.AddListener(OnToggleChanged);
+        for (int i = 0; i < toggles.Length; i++)
+        {
+            int index = i; // Store the current index for use in the lambda expression
+            toggles[index].onValueChanged.AddListener(isChecked => ToggleStrikethrough(index, isChecked));
+            toggles[index].onValueChanged.AddListener(OnToggleChanged); // No index needed here
+        }
     }
 
     private void OnToggleChanged(bool isChecked)
@@ -39,20 +43,20 @@ public class ToDoListManager : MonoBehaviour
         imageToChange.sprite = originalSprite;
     }
 
-    void ToggleStrikethrough(bool isChecked)
+    private void ToggleStrikethrough(int index, bool isChecked)
     {
-        TMP_Text textComponent = inputField.textComponent;
+        TMP_Text textComponent = inputFields[index].textComponent;
 
         if (isChecked)
         {
             // Apply strikethrough to the InputField text using TMP's built-in style
-            inputField.text = $"<s>{inputField.text}</s>";
+            inputFields[index].text = $"<s>{inputFields[index].text}</s>";
             textComponent.color = new Color(0.5f, 0.5f, 0.5f); // Optional: Dim color
         }
         else
         {
             // Remove strikethrough by re-assigning plain text without formatting
-            inputField.text = inputField.text.Replace("<s>", "").Replace("</s>", "");
+            inputFields[index].text = inputFields[index].text.Replace("<s>", "").Replace("</s>", "");
             textComponent.color = Color.black; // Restore color
         }
     }
